b31c95da9e585b32dff791e8fe37ee02d9aeb7ca
vibondarenko <vibondarenko@gmail.com>
2025-10-06 21:12:24 +0200

    Quartz sync: Oct 6, 2025, 9:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "vbodev.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", ],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
